e8b00c0fb117be411bb895aa794f2e0721708002
Christine Lee <cylee4@gmail.com>
2015-02-13 20:09:28 -0800

    (fix) Fixed premature gulp task exit

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,20 @@ var uglify = require('gulp-uglify');
 var notify = require('gulp-notify');
 var rename = require('gulp-rename');
 var karma = require('karma').server;
+var eventStream = require('event-stream');
+var order = require('gulp-order');
 // var watch = require('gulp-watch');
 
+var paths = {
+  src: {
+    main: 'client/src/*.js',
+    components: 'client/src/*/*.js'
+  },
+  karmaConf: __dirname + '/karma.conf.js'
+};
+
 var handleError = function(err) {
-  console.log('ERROR MSG:', error.toString());
+  console.log('ERROR MSG:', err.toString());
   this.emit('end');
 };
 
@@ -20,24 +30,35 @@ gulp.task('default', ['clean', 'test'], function() {
 
 // Jshint, concats, uglifies scripts into dist
 gulp.task('scripts', function() {
-  return gulp.src('src/scripts/*.js')
+  var appFiles = gulp.src(paths.src.main)
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/assets/js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify({
-      beautify: true
-    }))
     .on('error', handleError)
-    .pipe(gulp.dest('dist/assets/js'))
-    .pipe(notify({message: 'Scripts task complete'}));
+    .pipe(concat('app.temp.js'));
+
+  var componentFiles = gulp.src(paths.src.components)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .on('error', handleError)
+    .pipe(concat('components.temp.js'));
+
+  return eventStream.concat(appFiles, componentFiles)
+    .pipe(order([
+      'app.temp.js',
+      'components.temp.js'
+    ]))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('client/dist/assets/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('client/dist/assets/js'))
+    .pipe(notify({message: 'Tests and scripts task complete'}));
 });
 
 // Runs Karma tests
 gulp.task('test', function(done) {
   karma.start({
-    configFile: __dirname + '/karma.conf.js',
+    configFile: paths.karmaConf,
     singleRun: true
   }, function() {
     done();
@@ -49,9 +70,9 @@ gulp.task('clean', function(cb) {
   del(['dist/assets/js'], cb)
 });
 
-gulp.task('watch', function() {
-  // Create LiveReload server
-  // livereload.listen();
-  // gulp.watch(['dist/**']).on('change', livereload.changed);
-  gulp.watch('src/scripts/*.js', ['scripts']);
-});
+// gulp.task('watch', function() {
+//   // Create LiveReload server
+//   // livereload.listen();
+//   // gulp.watch(['dist/**']).on('change', livereload.changed);
+//   gulp.watch('src/scripts/*.js', ['scripts']);
+// });
