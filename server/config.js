3499eee6400040112215cefff2e72fb7726b764a
Christine Lee <cylee4@gmail.com>
2015-03-01 17:09:39 -0800

    item_id in req.params, fix indentation

--- server/config.js
+++ server/config.js
@@ -169,11 +169,12 @@ module.exports = function(app, express){
   });
 
   //API routes for reviews
-  app.post('/api/reviews/:user', reviews.create);
-  app.get('/api/reviews/user/:user', reviews.getReviews);
+  // Not sure if reviews.create is being used.
+  // app.post('/api/reviews/:userId', reviews.create);
+  app.get('/api/reviews/user/:userId', reviews.getReviews);
   app.put('/api/reviews/:reviewId/update', reviews.updateOne);
-  app.post('/api/reviews/users/:lender_id/:borrower_id', reviews.createPending); // [Note] Creates two reviews without rating/content for lender/borrower
-  app.get('/api/reviews/:user', reviews.getPendingReviews);
+  app.post('/api/reviews/users/:lender_id/:borrower_id/:item_id', reviews.createPending); // [Note] Creates two reviews without rating/content for lender/borrower
+  app.get('/api/reviews/:userId', reviews.getPendingReviews);
 
   //API routes for items (can amend as we decide what we need)
   app.post('/api/items/:userId', items.create); //WORKS
